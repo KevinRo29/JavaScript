308acb3679cb25d0d476ccbc57b8a4646efc327f
KevinRo29 <114008638+KevinRo29@users.noreply.github.com>
2023-08-14 11:53:15 -0600

    Comments

--- BMI Calculator/sources/js/main.js
+++ BMI Calculator/sources/js/main.js
@@ -1,3 +1,4 @@
+// DOM content loaded event
 document.addEventListener('DOMContentLoaded', function () {
   const calculateButton = document.getElementById('calculate');
   const weightInput = document.getElementById('weight');
@@ -7,19 +8,20 @@ document.addEventListener('DOMContentLoaded', function () {
   const bmiTable = document.getElementById('bmiTable');
 
   calculateButton.addEventListener('click', function () {
-    const weight = parseFloat(weightInput.value);
+    const weight = parseFloat(weightInput.value); // Convert weight to kg
     const height = parseFloat(heightInput.value) / 100; // Convert height to meters
-    if (isNaN(weight) || isNaN(height) || height === 0) {
+    if (isNaN(weight) || isNaN(height) || height === 0) { // Check if weight or height is not a number or height is 0
       resultDiv.textContent = 'Please enter valid weight and height.';
       bmiResultDiv.textContent = '';
     } else {
-      const bmi = weight / (height * height);
-      const classification = getClassification(bmi);
-      resultDiv.textContent = `Your BMI is: ${bmi.toFixed(2)}`;
-      bmiResultDiv.textContent = `Classification: ${classification}`;
+      const bmi = weight / (height * height); // Calculate BMI
+      const classification = getClassification(bmi); // Get classification
+      resultDiv.textContent = `Your BMI is: ${bmi.toFixed(2)}`; // Display BMI
+      bmiResultDiv.textContent = `Classification: ${classification}`; // Display classification
     }
   });
 
+  // Get classification based on BMI
   function getClassification(bmi) {
     if (bmi < 18.5) {
       return 'Underweight';
